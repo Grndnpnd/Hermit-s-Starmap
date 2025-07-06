23dbdc0a9cb9b6fe568dd788b56bc7d4848a9fbc
Grndnpnd <j.pella.bgb@gmail.com>
2025-07-06 00:01:29 -0700

    Update enhanced_starmap.js

--- js/enhanced_starmap.js
+++ js/enhanced_starmap.js
@@ -512,7 +512,7 @@ class EnhancedStarMap {
     
     generateEnhancedStarField() {
         this.stars = [];
-        const numStars = this.performanceMode ? 1200 : 3500;
+        const numStars = this.performanceMode ? 2500 : 3500;
         
         for (let i = 0; i < numStars; i++) {
             const angle = Math.random() * Math.PI * 2;
